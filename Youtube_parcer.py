14d0db0c248e05f15d10b552d17dd667e9c41718
Nikita <draco1997pst@gmail.com>
2023-11-29 20:55:32 +0200

    start

---
+++ Youtube_parcer.py
@@ -0,0 +1,54 @@
+import json
+from googleapiclient.discovery import build
+import google.oauth2
+import sqlite3
+import config
+
+class Youtube():
+    def __init__(self, API_KEY, pathDb, ID_CHANEL):
+        self.API_KEY = API_KEY
+        self.pathDb = pathDb
+        self.ID_CHANEL = ID_CHANEL
+
+    def getlist(self):
+        keyUrl = "https://www.youtube.com/watch?v="
+
+        service = build('youtube', 'v3', developerKey=self.API_KEY)
+
+        r = service.search().list(
+            channelId=self.ID_CHANEL,
+            part="snippet",
+            type="video",
+            order="date",
+            maxResults='50'
+        ).execute()
+
+        dat = r['items'][0]['snippet']['publishedAt']
+        title = r['items'][0]['snippet']['title']
+        l = r['items']
+        youYubeList = []
+
+        for i in l:
+            titleChanel = i['snippet']['channelTitle']
+            title = i['snippet']['title'].replace("&quot;", "")
+
+            dataPublish = i['snippet']['publishedAt'].split('T')[0]
+            url = keyUrl + i['id']['videoId']
+
+            key = i['id']['videoId']
+            #
+            youYubeList.append((dataPublish, title, url, titleChanel, key))
+
+        return youYubeList
+
+
+
+
+
+
+if __name__ == '__main__':
+    pass
+    # do = Youtube(config.API_KEY2, config.db, config.NM)
+    #
+    # for i in do.getlist():
+    #     print(i)
---
+++ get.py
@@ -0,0 +1,7 @@
+def getId(name):
+    for i in list:
+        for j in i:
+            if j == name.lower():
+                return list.index(i)
+
+
