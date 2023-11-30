18230db5d72b783f4b6944815021fa98269931ba
Nikita <draco1997pst@gmail.com>
2023-11-30 15:52:46 +0200

    last object

---
+++ folderParcer.py
@@ -0,0 +1,41 @@
+import os
+import config
+
+
+
+class Folder():
+
+    def __init__(self, path):
+        self.path = path
+        self.l = []
+        self.g =[]
+
+    def WalkFiles(self,current_path=None):
+        if current_path is None:
+            current_path = self.path
+
+        for i in os.listdir(current_path):
+
+            if os.path.isdir(current_path + '/' + i):
+                self.WalkFiles(current_path + '/' + i)
+            else:
+                self.l.append(i)
+        return self.l
+
+    def getListFiles(self):
+
+        for i in self.WalkFiles(self.path):
+            s = i.replace(".mp4", "")
+            s = s.split('_', 4)
+            t = len(s)
+            if t == 5:
+                self.g.append(s)
+        return self.g
+
+
+if __name__ =='__main__':
+    # pass
+    folder = Folder(config.path)
+    # print(folder.getListFiles(config.path))
+    for i in folder.getListFiles():
+        print(i)
