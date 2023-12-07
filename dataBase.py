e3bf199d1f54944f378d0fd8755e753043e90c64
Nikita <draco1997pst@gmail.com>
2023-12-07 16:59:44 +0200

    ADD def createDb def createTables def writheBdYoutube

--- dataBase.py
+++ dataBase.py
@@ -1,5 +1,5 @@
 import sqlite3
-
+import Youtube_parcer
 import folderParcer
 import get
 import config
@@ -8,77 +8,91 @@ class dataBase():
     def __init__(self, pathDb):
         self.pathDb = pathDb
 
+    def createDb(self, path = None):
+        if path == None:
+            path = self.pathDb
 
-    def createTableYoutube(self):
-        db = sqlite3.connect(self.pathDb)
-        c = db.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS YoutubeVideo 
-           (data DATA, 
-           name NVARCHAR(256), 
-           url TEXT, 
-           chanel TEXT,
-           key TEXT)''')
+        db = sqlite3.connect(path)
 
         db.commit()
+        print("DB OK!")
 
-    def createTableNAS(self):
 
-        db = sqlite3.connect(self.pathDb)
+    def createTables(self, path = None):
+
+        if path == None:
+            path = self.pathDb
+
+        db = sqlite3.connect(path)
         c = db.cursor()
-        c.execute('''CREATE TABLE IF NOT EXISTS VideoStorage
-        (
-        NameFile TEXT,
-        TypeClip TEXT,
-        NameJurn TEXT,
-        NameDirector TEXT,
-        key TEXT,
-        FOREIGN KEY (key) REFERENCES YoutubeVideo(key)
 
-        )''')
+        c.execute('''
+        
+                CREATE TABLE IF NOT EXISTS Youtube
+                (
+                publishData DATA,
+                FileName TEXT,
+                chanel TEXT,
+                key TEXT
+
+        );''')
+
+        c.execute('''
+
+                CREATE TABLE IF NOT EXISTS FilesOnNas
+                (
+                renderData DATA,
+                fileName TEXT,
+                ClipType INT,
+                Newsman INT,
+                Editor INT,
+                key TEXT
+                );''')
+
+        c.execute('''
+                CREATE TABLE IF NOT EXISTS emploues 
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name TEXT
+                );''')
+
+        c.execute('''
+                CREATE TABLE IF NOT EXISTS VideoType
+                (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                Name TEXT
+                );''')
+
+
+
 
         db.commit()
+        print("TABLES CREATE COMPLITE!")
 
-    def writheBdYoutube(self, list):
-        db = sqlite3.connect(self.pathDb)
+    def writheBdYoutube(self, list, path = None):
+        if path == None:
+            path = self.pathDb
+
+        db = sqlite3.connect(path)
         c = db.cursor()
+
         for i in list:
-            c.execute(f"SELECT name FROM YoutubeVideo WHERE name = '{i[1]}'")
+            c.execute(f"SELECT FileName FROM Youtube WHERE FileName = '{i[1]}'")
             if c.fetchone() is None:
-                c.execute(f"INSERT INTO YoutubeVideo VALUES (?, ?, ?, ?, ?)", (i[0], i[1], i[2], i[3], i[4]))
-        db.commit()
+                c.execute(f"INSERT INTO Youtube VALUES (?, ?, ?, ?)", (i[0], i[1], i[2],i[3]))
+            db.commit()
 
-    def writheBdListVideostorage(self, list):
-        db = sqlite3.connect(self.pathDb)
-        c = db.cursor()
-        for i in list:
-
-            nameDir = i[3]
-            nameJurn = i[2]
-
-            typVideo = i[1]
-
-            key = i[4].replace(" ", "")
-            key = key.replace("@SynoEAStream", "")
-            key = key.replace("@SynoResource", "")
-
-            if get.getId(nameJurn, config.list) != None:
-                nameJurn = get.getId(nameJurn, config.list)
-            if get.getId(nameDir, config.list) != None:
-                nameDir = get.getId(nameDir, config.list)
-            if get.getId(typVideo) != None:
-                typVideo = get.getId(typVideo)
-
-            if typVideo == "НМ" or typVideo == "ДО" or typVideo == "ДП":
-                typVideo = "СЮЖ"
-
-            c.execute(f"SELECT key FROM VideoStorage WHERE key = '{key}'")
-            if c.fetchone() is None:
-                c.execute(f"INSERT INTO VideoStorage VALUES (?, ?, ?, ?, ?)", (i[0], typVideo, nameJurn, nameDir, key))
-        db.commit()
 
 
 
 if __name__ == '__main__':
+    db = dataBase(config.db)
+    do = Youtube_parcer.Youtube(config.API_KEY2, config.DO)
+    # for i in do.getlist():
+    #     print(i)
+    # db.writheBdYoutube(do.getlist())
 
-    pass
+    # db.writheBdYoutube()
+
+
+
 
